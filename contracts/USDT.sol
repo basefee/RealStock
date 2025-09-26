9062db8e50d61483516a93fd81226c2cbbf83738
basefee <garvagarwal2003@gmail.com>
2025-09-27 04:08:36 +0530

    rootstock contracts

---
+++ contracts/USDT.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/// @title MockUSDT
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract USDT is ERC20 {
+    constructor() ERC20("USDT", "USDT") {    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
